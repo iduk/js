98c4dd53f0b66c8a193d010d2be2b29b8d1acf91
iduk <iduk.design@gmail.com>
2019-10-22 22:58:17 +0900

    .

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+const withCSS = require('@zeit/next-css')
+const withSass = require('@zeit/next-sass')
+
+module.exports = withSass({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: '[name]__[local]--[hash:base64:5]',
+  },
+  postcssLoaderOptions: {
+    parser: true,
+  },
+})
