535fb4ad013a8ce405e89221a25dd33e390b4649
iduk <iduk.design@gmail.com>
2019-10-23 11:27:18 +0900

    ...testing

--- pages/page1.js
+++ pages/page1.js
@@ -1,18 +1,50 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'next/link'
 import Head from 'next/head'
 import Nav from '../components/nav/nav'
 
-const page1 = () => (
-  <main>
-    <Head>
-      <title>Page1</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+class Page1 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loaded: false,
+    }
+  }
+  componentDidMount() {
+    this.timeoutId = setTimeout(() => {
+      this.props.pageTransitionReadyToEnter()
+      this.setState({ loaded: true })
+    }, 2000)
+  }
+  componentWillUnmount() {
+    if (this.timeoutId) clearTimeout(this.timeoutId)
+  }
 
-    <Nav />
+  render() {
+    if (!this.state.loaded) return null
+    return (
+      <div>
+        <Nav />
+        <h1>Page1</h1>
+        <p>
+          Notice how a loading spinner showed up while my content was "loading"?
+          Pretty neat, huh?
+        </p>
+        <Link href="/">
+          <a className="btn btn-light">Go back home</a>
+        </Link>
+      </div>
+    )
+  }
+}
 
-    <h1>page1</h1>
-  </main>
-)
+Page1.propTypes = {
+  pageTransitionReadyToEnter: PropTypes.func,
+}
 
-export default page1
+Page1.defaultProps = {
+  pageTransitionReadyToEnter: () => {},
+}
+
+export default Page1
--- pages/page2.js
+++ pages/page2.js
@@ -1,18 +1,32 @@
+import App, { Container } from 'next/app'
 import React from 'react'
-import Head from 'next/head'
+import { PageTransition } from 'next-page-transitions'
 import Nav from '../components/nav/nav'
 
-const page2 = () => (
-  <main>
-    <Head>
-      <title>Page2</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <Nav />
-
-    <h1>page2</h1>
-  </main>
-)
-
-export default page2
+export default class Page2 extends React.Component {
+  render() {
+    return (
+      <section>
+        <Nav />
+        <h1>asdfasdf</h1>
+        asdfasdfasdf
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transition: opacity 300ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+        `}</style>
+      </section>
+    )
+  }
+}
