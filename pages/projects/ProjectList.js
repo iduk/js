850d1f17a7b2c05d17a6e57bdd432a7df7d28443
iduk <iduk.design@gmail.com>
2022-07-18 04:20:49 +0900

    .

--- pages/projects/ProjectList.js
+++ pages/projects/ProjectList.js
@@ -1,36 +1,32 @@
-import { useRouter, useState } from "next/router"
 import Link from "next/link"
-import axios from "axios"
+// import axios from "axios"
 import posts from "../../data/mock_data.json"
 
 const ProjectList = () => {
   return (
-    <>
-      <ol className="post-list">
-        {posts.map((post, index) => (
-          <li key={post.id}>
-            {/* <Link href={`/projects/article${post.id}`}> */}
-            <Link href={"#"}>
-              <a className="post-link">
-                <div className="contents">
-                  <small className="mt-2">
-                    {post.date} ⋅ {post.skill}
-                  </small>
-                  <p className="display-3 title">{post.title}</p>
-                  <p className="desc small">{post.desc}</p>
-                </div>
-                <span
-                  className="thumb mask"
-                  style={{
-                    backgroundImage: `url(${post.thumbImg})`,
-                  }}
-                />
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ol>
-    </>
+    <ol className="post-list">
+      {posts.map((post, index) => (
+        <li key={post.id}>
+          <Link href={"#"}>
+            <a className="post-link">
+              <div className="contents">
+                <small className="mt-2">
+                  {post.date} ⋅ {post.skill}
+                </small>
+                <p className="display-3 title">{post.title}</p>
+                <p className="desc small">{post.desc}</p>
+              </div>
+              <span
+                className="thumb mask"
+                style={{
+                  backgroundImage: `url(${post.thumbImg})`,
+                }}
+              />
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ol>
   )
 }
 
