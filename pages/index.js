36508eed83187e46cc5f2c2c030d9e51e735e96e
iduk <iduk.design@gmail.com>
2022-07-18 14:00:18 +0900

    Index 레이아웃 변경.

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import posts from "../data/mock_data.json"
 import Marquee from "react-fast-marquee"
 import { gsap, Power1, Power2 } from "gsap"
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
+import Link from "next/link"
 gsap.registerPlugin(ScrollTrigger)
 
 const Logos = [
@@ -24,6 +25,25 @@ function Index() {
   const postsList = posts.slice(0, 4)
 
   useEffect(() => {
+    // document.addEventListener("DOMContentLoaded", function () {
+    //   gsap.utils.toArray(".text-outline").forEach(function (textLayer) {
+    //     hide(textLayer)
+
+    //     ScrollTrigger.create({
+    //       trigger: textLayer,
+    //       onEnter: function () {
+    //         animateFrom(textLayer)
+    //       },
+    //       onEnterBack: function () {
+    //         animateFrom(textLayer, -1)
+    //       },
+    //       onLeave: function () {
+    //         hide(textLayer)
+    //       },
+    //     })
+    //   })
+    // })
+
     // gsap scrollTrigger
     gsap.utils.toArray(".page-index").forEach((section) => {
       const elems = section.querySelectorAll("section")
@@ -82,11 +102,24 @@ function Index() {
             <div className="feature--header">
               {/* 1 */}
               <div>
-                <p className="h5 text-uppercase">Award 2023</p>
-                <p className="headline text-uppercase">
-                  OPENFLOOR’s Webb Space Telescope Reveals Astounding,
-                  Unprecedented Views of the Universe
-                </p>
+                {/* <p className="h5 text-uppercase">Award 2023</p> */}
+                <div className="headline text-uppercase">
+                  <Link href="/">
+                    <a>OPENFLOOR</a>
+                  </Link>
+                  <Link href="/">
+                    <a>Multi-Platform</a>
+                  </Link>
+                  <Link href="/">
+                    <a>Data to Information</a>
+                  </Link>
+                  <Link href="/">
+                    <a>Device Handling</a>
+                  </Link>
+                  <Link href="/">
+                    <a>Everyday Coding</a>
+                  </Link>
+                </div>
               </div>
               {/* \\ */}
 
@@ -100,81 +133,102 @@ function Index() {
         </section>
 
         {/* image list */}
-        <section className="container-fluid my-4">
-          <ul className="row align-content-stretch">
-            <li className="col-12 col-lg-12">
-              <div className="section-bg mask">
-                <div className="foreground text-center">
-                  <p className="text-outline">Openfloor Openfloor</p>
-                  <p className="text-fill">Openfloor</p>
+        <section className="centered">
+          {/* back */}
+          <div className="container-fluid background my-6">
+            <article className="row no-gutters">
+              <div
+                className="col-12 col-md-6 border centered section-bg"
+                style={{
+                  backgroundImage: "url(http://placebeard.it/1200/800)",
+                }}
+              ></div>
+              <div className="col-12 col-md-6 border border-lg-left-0 centered"></div>
+            </article>
+          </div>
+
+          {/* fore */}
+          <div className="container foreground my-6">
+            <ul className="row justify-content-between align-content-stretch">
+              <li className="col-12 col-md-6 centered">1</li>
+              <li className="col-12 col-md-6">
+                <div className="p-lg-5 p-3">
+                  <h3 className="pb-3">
+                    People with similar interests and goals come together to
+                    change the world for the better.
+                  </h3>
+                  <p>
+                    시들어 무엇을 기관과 싶이 얼음과 봄바람이다. 가진 타오르고
+                    속잎나고, 힘차게 과실이 것이다. 원대하고, 미묘한 가는 피가
+                    아니다. 목숨이 것은 보이는 피에 착목한는 청춘을 너의 보라.
+                    끝에 거친 우리 방황하여도, 찬미를 만천하의 무엇을 밥을
+                    기관과 사막이다. 못하다 끝까지 끓는 안고, 두기 그들의 쓸쓸한
+                    투명하되 것이다. 이것이야말로 미묘한 얼음 그것을 관현악이며,
+                    생명을 이것을 속잎나고, 말이다. 보이는 피어나는 뭇 싶이
+                    칼이다. 이상, 영락과 것은 보라. 충분히 이 너의 풀이 찬미를
+                    실현에 안고, 고행을 하였으며, 사막이다.
+                  </p>
                 </div>
-              </div>
-            </li>
-          </ul>
+              </li>
+            </ul>
+          </div>
         </section>
 
-        <section className="container-fluid my-4">
-          <article className="row no-gutters">
-            <div className="col-12 col-lg-4">
-              <div className="p-4 mask bg-black text-white">
-                <p className="h4">what we work.</p>
-                <div
-                  className="display-2 d-flex align-items-center"
-                  style={{ height: "35vh" }}
-                >
-                  <p className="display-2 text-center py-3">
-                    <span className="badge badge-pill border m-1">
-                      Technology
-                    </span>
-                    <span className="badge badge-pill border m-1">
-                      Innovation
-                    </span>
-                    <span className="badge badge-pill border m-1">
-                      Affection
-                    </span>
-                  </p>
-                  <p></p>
+        <section className="container-fluid py-6 bg-gray">
+          <div className="container my-4">
+            <article className="row">
+              <div className="col-12 col-md-4">
+                <div className="p-4 mask bg-black text-white">
+                  <p className="h4">what we work.</p>
+                  <div
+                    className="display-2 d-flex align-items-center"
+                    style={{ height: "35vh" }}
+                  >
+                    <p className="display-2 text-center py-3">
+                      Inderstrial Design
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-12 col-lg-4">
-              <div className="p-4 mask bg-black text-white">
-                <p className="h4">what we work.</p>
-                <div
-                  className="display-2 text-center d-flex align-items-center justify-content-center"
-                  style={{ height: "35vh" }}
-                >
-                  <p className="display-2 text-center py-3">
-                    <span className="badge badge-pill  border m-1">
-                      Technology
-                    </span>
-                    <span className="badge badge-pill border m-1">
-                      Innovation
-                    </span>
-                    <span className="badge badge-pill border m-1">
-                      Affection
-                    </span>
-                  </p>
-                  <p></p>
+              <div className="col-12 col-md-4">
+                <div className="p-4 mask bg-light">
+                  <p className="h4">what we work.</p>
+                  <div
+                    className="display-2 text-center d-flex align-items-center justify-content-center"
+                    style={{ height: "35vh" }}
+                  >
+                    <p className="display-2 text-center py-3">
+                      <span className="badge badge-pill  border m-1">
+                        Technology
+                      </span>
+                      <span className="badge badge-pill border m-1">
+                        Innovation
+                      </span>
+                      <span className="badge badge-pill border m-1">
+                        Affection
+                      </span>
+                    </p>
+                    <p></p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-12 col-lg-4">
-              <div className="p-4 mask bg-black text-white">
-                <p className="h4">what we work.</p>
-                <div
-                  className="display-2 text-center d-flex align-items-center justify-content-center"
-                  style={{ height: "35vh" }}
-                >
-                  <p className="display-2 text-center py-3">
-                    <span className="badge badge-pill  border m-1">
-                      Technology
-                    </span>
-                  </p>
+              <div className="col-12 col-md-4">
+                <div className="p-4 mask bg-black text-white">
+                  <p className="h4">what we work.</p>
+                  <div
+                    className="display-2 text-center d-flex align-items-center justify-content-center"
+                    style={{ height: "35vh" }}
+                  >
+                    <p className="display-2 text-center py-3">
+                      <span className="badge badge-pill  border m-1">
+                        Technology
+                      </span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </article>
+            </article>
+          </div>
         </section>
 
         <section className="container py-8">
